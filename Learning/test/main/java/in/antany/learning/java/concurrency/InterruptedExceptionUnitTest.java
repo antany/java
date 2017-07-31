0b44220eda5f938af2a706b1b856d4abcc11e2cd
Antany <antany84@gmail.com>
2017-07-30 22:10:54 -0400

    unit test added for interrupted exception

---
+++ Learning/test/main/java/in/antany/learning/java/concurrency/InterruptedExceptionUnitTest.java
@@ -0,0 +1,14 @@
+package in.antany.learning.java.concurrency;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class InterruptedExceptionUnitTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
